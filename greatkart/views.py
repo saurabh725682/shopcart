5a072ae6885c136ca6adc1a9acea2ccc25a4a4ca
Saurabh Maurya <saurabhmaurya725682@gmail.com>
2025-03-15 09:16:00 +0530

    Adding greatkart project

---
+++ greatkart/views.py
@@ -0,0 +1,59 @@
+from django.shortcuts import render
+from store.models import Product, ReviewRating
+
+def home(request):
+    products = Product.objects.all().filter(is_available=True)
+    
+    # Get the reviews
+    for product in products:
+        reviews = ReviewRating.objects.filter(product_id=product.id, status=True)
+    
+    context = {'products': products,
+               'reviews': reviews}
+    
+    return render(request, 'home.html', context)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
