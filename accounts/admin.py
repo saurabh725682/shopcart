dcb60c5d996fa3b5f9b586b8dae2ceacf0f34327
Saurabh Maurya <saurabhmaurya725682@gmail.com>
2025-04-17 16:41:41 +0530

    Update admin.py

--- accounts/admin.py
+++ accounts/admin.py
@@ -5,6 +5,7 @@ from django.utils.html import format_html
 
 # Register your models here.
 
+# Custom admin interface for the Account model
 class AccountAdmin(UserAdmin):
     list_display = ('email','first_name', 'last_name', 'username', 'last_login', 'date_joined', 'is_active')
     list_display_links = ('email', 'first_name', 'last_name')
@@ -14,13 +15,16 @@ class AccountAdmin(UserAdmin):
     filter_horizontal = ()
     list_filter = ()
     fieldsets = () 
-    
 
+
+# Custom admin interface for the UserProfile model
 class UserProfileAdmin(admin.ModelAdmin):
     def thumbnail(self, object):
         return format_html('<img src="{}" width="30" style="border-radius:50%;">'.format(object.profile_picture.url))
     thumbnail.short_description = 'Profile Picture'
     list_display = ('thumbnail', 'user', 'city', 'state', 'country')
 
+
+# Register the custom admin interfaces with the admin site
 admin.site.register(Account, AccountAdmin)
 admin.site.register(UserProfile, UserProfileAdmin)
