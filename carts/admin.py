74e7f6a688b2ba76126a139d090972083955d0ae
Saurabh Maurya <saurabhmaurya725682@gmail.com>
2025-04-17 17:02:24 +0530

    Update admin.py

--- carts/admin.py
+++ carts/admin.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
 from .models import *
 
-# Register your models here.
-
+# Custom admin interface for the Cart model
 class CartAdmin(admin.ModelAdmin):
     list_display = ('cart_id', 'date_added')
-    
+
+# Custom admin interface for the CartItem model
 class CartItemAdmin(admin.ModelAdmin):
     list_display =('product', 'cart', 'quantity', 'is_active')
 
+# Register your models here
 admin.site.register(Cart, CartAdmin)
 admin.site.register(CartItem, CartItemAdmin)
