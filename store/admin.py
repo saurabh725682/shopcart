1d87a55a93c3d46fa2476eeb7d46ebe7cdb6029b
Saurabh Maurya <saurabhmaurya725682@gmail.com>
2025-04-17 20:53:29 +0530

    Update admin.py

--- store/admin.py
+++ store/admin.py
@@ -6,12 +6,14 @@ import admin_thumbnails
 class ProductGalleryInline(admin.TabularInline):
     model = ProductGallery
     extra = 1
-    
+
+# Custom admin interface for the Product model
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('product_name', 'price', 'stock', 'category', 'modified_date', 'is_available')
     prepopulated_fields = {'slug': ('product_name',)}
     inlines = [ProductGalleryInline]
-    
+
+# Custom admin interface for the Variation model
 class VariationAdmin(admin.ModelAdmin):
     list_display = ('product', 'variation_category', 'variation_value', 'is_active')
     list_editable = ('is_active',)
